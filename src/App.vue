a8b2f84ecfd840c9a7c36f623eb6127b96f740f4
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-05-04 17:57:10 +0300

    delete unnecessary components and starts making todo app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+
+const todos = ref([])
+const name = ref('')
+const input_content = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(()=> todos.value.sort((a,b)=> {
+  return a.createdAt - b.createdAt
+}))
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="name here" v-model="name">
+      </h2>
+    </section>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
